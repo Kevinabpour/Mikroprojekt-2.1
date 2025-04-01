1dcb1b73c5456bc2a3cf30e95bcbbaebc80a87c6
mal025 <mal025@edu.zealand.dk>
2025-04-01 12:04:30 +0200

    Added checkmarks for the equipment in each room

--- Repo/RoomCollectionRepo.cs
+++ Repo/RoomCollectionRepo.cs
@@ -7,14 +7,14 @@ namespace Mikroprojekt_2.Repo
         List<Room> _rooms = new List<Room>();
         public RoomCollectionRepo() // int roomID, string name, string description, int capacity, List<string> equipment
         {
-            _rooms.Add(new Room(101, "room1", "description 1", 35, ["equipment1", "equipment2"]));
-            _rooms.Add(new Room(102, "room2", "description 2", 10, ["equipment1", "equipment2"]));
-            _rooms.Add(new Room(103, "room3", "description 3", 50, ["equipment1", "equipment2"]));
-            _rooms.Add(new Room(104, "room4", "description 4", 20, ["equipment1", "equipment2"]));
-            _rooms.Add(new Room(104, "room5", "description 5", 25, ["equipment1", "equipment2"]));
-            _rooms.Add(new Room(104, "room6", "description 6", 5, ["equipment1", "equipment2"]));
-            _rooms.Add(new Room(104, "room7", "description 7", 40, ["equipment1", "equipment2"]));
-            _rooms.Add(new Room(104, "room8", "description 8", 500, ["equipment1", "equipment2"]));
+            _rooms.Add(new Room(101, "room1", "description 1", 35, ["Smartboard", "Forplejning"]));
+            _rooms.Add(new Room(102, "room2", "description 2", 10, []));
+            _rooms.Add(new Room(103, "room3", "description 3", 50, ["Smartboard"]));
+            _rooms.Add(new Room(104, "room4", "description 4", 20, ["Forplejning"]));
+            _rooms.Add(new Room(104, "room5", "description 5", 25, ["Smartboard"]));
+            _rooms.Add(new Room(104, "room6", "description 6", 5, ["Smartboard", "Forplejning"]));
+            _rooms.Add(new Room(104, "room7", "description 7", 40, ["Smartboard", "Forplejning"]));
+            _rooms.Add(new Room(104, "room8", "description 8", 500, ["Smartboard"]));
         } 
         public List<Room> GetAll()
         {
