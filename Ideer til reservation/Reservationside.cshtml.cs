d0678ecf2e2b37135aab2058801a8c99bf522c1d
Christian F <68164706+Krill1302@users.noreply.github.com>
2025-04-01 13:20:16 +0200

    Har lavet et teste format til når der skal reserveres tid

---
+++ Ideer til reservation/Reservationside.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Mikroprojekt_2.Ideer_til_reservation
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
