fd3a6ad679a6ee8d95a89f850514b7fe78c74237
mal025 <mal025@edu.zealand.dk>
2025-04-03 15:24:51 +0200

    Added user class

---
+++ Model/User.cs
@@ -0,0 +1,28 @@
+﻿namespace Mikroprojekt_2.Model
+{
+    public class User
+    {
+        public int UserID { get; set; }
+        public string UserName { get; set; }
+        public List<int> BookingList { get; }
+
+        public User()
+        {
+
+        }
+        public User(int userID, string userName)
+        {
+            UserID = userID;
+            UserName = userName;
+        }
+
+        public void AddBooking(int BookingNum)
+        {
+            BookingList.Add(BookingNum);
+        }
+        public void RemoveBooking(int BookingNum)
+        {
+        }
+
+    }
+}
