8ab21e207c1aff4d939e603123419edddcc866ff
mal025 <mal025@edu.zealand.dk>
2025-04-03 15:50:23 +0200

    Created bookingservice

---
+++ Services/BookingService.cs
@@ -0,0 +1,36 @@
+﻿using Mikroprojekt_2.Model;
+using Mikroprojekt_2.Repo;
+
+namespace Mikroprojekt_2.Services
+{
+    public class BookingService
+    {
+        private IBookingRepo _bookingRepo;
+
+        public BookingService(IBookingRepo bookingRepo)
+        {
+            _bookingRepo = bookingRepo;
+        }
+        public List<Booking> GetAll()
+        {
+            return _bookingRepo.GetAll();
+        }
+        public Booking GetByID(int id)
+        {
+            return _bookingRepo.GetByID(id);
+        }
+        public void CreateBooking(Booking booking)
+        {
+            _bookingRepo.CreateBooking(booking);
+        }
+        public void RemoveBooking(int id)
+        {
+            _bookingRepo.RemoveBooking(id);
+        }
+        public void UpdateBooking(int id, string[] time, string comment)
+        {
+            _bookingRepo.UpdateBooking(id, time, comment);
+        }
+
+    }
+}
